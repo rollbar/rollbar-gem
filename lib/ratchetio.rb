3ec1b4a58a791359fd1e9b25a462ba0e5fd8cc1e
Brian Rue <brian@ratchet.io>
2012-11-20 16:36:46 -0800

    add some yard-style documentation for public methods in the Ratchetio class

--- lib/ratchetio.rb
+++ lib/ratchetio.rb
@@ -10,14 +10,40 @@ module Ratchetio
   class << self
     attr_writer :configuration
     
+    # Configures the gem.
+    # Call on app startup to set the `access_token` (required) and other config params.
+    # In a Rails app, this is called by `config/initializers/ratchetio.rb` which is generated
+    # with `rails generate ratchetio access-token-here`
+    #
+    # @example
+    #   Ratchetio.configure do |config|
+    #     config.access_token = 'abcdefg'
+    #   end
     def configure
       yield(configuration)
     end
 
+    # Returns the configuration object.
+    #
+    # @return [Ratchetio::Configuration] The configuration object
     def configuration
       @configuration ||= Configuration.new
     end
 
+    # Reports an exception to Ratchet.io
+    #
+    # @example
+    #   begin
+    #     foo = bar
+    #   rescue => e
+    #     Ratchetio.report_exception(e)
+    #   end
+    #
+    # @param exception [Exception] The exception object to report
+    # @param request_data [Hash] Data describing the request. Should be the result of calling 
+    #   `ratchetio_request_data`.
+    # @param person_data [Hash] Data describing the affected person. Should be the result of calling
+    #   `ratchetio_person_data`
     def report_exception(exception, request_data = nil, person_data = nil)
       filtered_level = configuration.exception_level_filters[exception.class.name]
       if filtered_level == 'ignore'
@@ -35,6 +61,17 @@ module Ratchetio
       logger.error "[Ratchet.io] Error reporting exception to Ratchet.io: #{e}"
     end
 
+    # Reports a message to Ratchet.io
+    #
+    # @example
+    #   Ratchetio.report_message("User login failed", 'info', :user_id => 123)
+    #
+    # @param message [String] The message body. This will be used to identify the message within 
+    #   Ratchet. For best results, avoid putting variables in the message body; pass them as 
+    #   `extra_data` instead. 
+    # @param level [String] The level. One of: 'critical', 'error', 'warning', 'info', 'debug'
+    # @param extra_data [Hash] Additional data to include alongside the body. Don't use 'body' as 
+    #   it is reserved.
     def report_message(message, level = 'info', extra_data = {})
       data = base_data(level)
       
--- ratchetio.gemspec
+++ ratchetio.gemspec
@@ -16,4 +16,6 @@ Gem::Specification.new do |gem|
   gem.version       = Ratchetio::VERSION
 
   #gem.add_development_dependency "rspec", "~> 2.6"
+  gem.add_development_dependency "yard"
+  gem.add_development_dependency "redcarpet"
 end
