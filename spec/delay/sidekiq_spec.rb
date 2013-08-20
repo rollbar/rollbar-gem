d70db79b9fbb887b0a8a9a249926ec6afa5afefc
Jérémy <jeremyvdw@gmail.com>
2013-08-20 12:09:10 +0200

    Fixed custom options passed to Rollbar::Configuration#use_sidekiq

---
+++ spec/delay/sidekiq_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require 'rollbar/delay/sidekiq'
+
+describe Rollbar::Delay::Sidekiq do
+  before(:each) do
+    reset_configuration
+  end
+
+  describe ".handler" do
+    let(:payload) { anything }
+
+    context "with default options" do
+      before { Rollbar.configuration.use_sidekiq = true }
+
+      it "enqueues to default queue" do
+        ::Sidekiq::Client.should_receive(:push).with(Rollbar::Delay::Sidekiq::OPTIONS.merge('args' => payload))
+        Rollbar::Delay::Sidekiq.handle(payload)
+      end
+    end
+
+    context "with custom options" do
+      let(:custom_config) { { 'queue' => 'custom_queue' } }
+
+      before { Rollbar.configuration.use_sidekiq = custom_config }
+
+      it "enqueues to custom queue" do
+        options = Rollbar::Delay::Sidekiq::OPTIONS.merge(custom_config.merge('args' => payload))
+        ::Sidekiq::Client.should_receive(:push).with options
+
+        Rollbar::Delay::Sidekiq.handle(payload)
+      end
+    end
+  end
+end
