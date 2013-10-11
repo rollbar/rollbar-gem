12fd1b8127dd24005a34731a9fc615a523652b71
Florent Guilleux <florent2@gmail.com>
2013-10-10 19:31:15 -0500

    Remove trailing spaces

--- lib/generators/rollbar/templates/initializer.rb
+++ lib/generators/rollbar/templates/initializer.rb
@@ -31,7 +31,7 @@ Rollbar.configure do |config|
   #
   # You can also specify a callable, which will be called with the exception instance.
   # config.exception_level_filters.merge!('MyCriticalException' => lambda { |e| 'critical' })
-  
+
   # Enable asynchronous reporting (uses girl_friday or Threading if girl_friday
   # is not installed)
   # config.use_async = true
