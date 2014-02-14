2843891ea45c809a0338ed7851f344c19b91c6a9
Chris Bielinski <chris@shadow.io>
2014-02-13 21:55:36 -0800

    fix typo in generated initializer

--- lib/generators/rollbar/templates/initializer.rb
+++ lib/generators/rollbar/templates/initializer.rb
@@ -2,7 +2,7 @@ require 'rollbar/rails'
 Rollbar.configure do |config|
   config.access_token = <%= access_token_expr %>
 
-  # Without configuration, Rollbar is enabled by in all environments. 
+  # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
   # Here we'll disable in 'test':
   if Rails.env.test?
