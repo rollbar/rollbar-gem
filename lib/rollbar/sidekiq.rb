0aaf352dcaaa9184b500308cc1bd18a0d59571f0
Richard Lee <dlackty@gmail.com>
2014-03-29 16:50:22 +0800

    Implement Sidekiq 3 error_handler

--- lib/rollbar/sidekiq.rb
+++ lib/rollbar/sidekiq.rb
@@ -1,25 +1,38 @@
 # encoding: utf-8
 
-module Rollbar
-  class Sidekiq
-    def call(worker, msg, queue)
-      begin
-        yield
-      rescue Exception => e
-        msg.delete('backtrace')
-        msg.delete('error_backtrace')
-        msg.delete('error_message')
-        msg.delete('error_class')
+if Sidekiq::VERSION < '3'
+  module Rollbar
+    class Sidekiq
+      def call(worker, msg, queue)
+        begin
+          yield
+        rescue Exception => e
+          msg.delete('backtrace')
+          msg.delete('error_backtrace')
+          msg.delete('error_message')
+          msg.delete('error_class')
 
-        Rollbar.report_exception(e, :params => msg)
-        raise
+          Rollbar.report_exception(e, :params => msg)
+          raise
+        end
       end
     end
   end
-end
 
-Sidekiq.configure_server do |config|
-  config.server_middleware do |chain|
-    chain.add Rollbar::Sidekiq
+  Sidekiq.configure_server do |config|
+    config.server_middleware do |chain|
+      chain.add Rollbar::Sidekiq
+    end
+  end
+else
+  Sidekiq.configure_server do |config|
+    config.error_handlers << Proc.new do |e, context|
+      context.delete('backtrace')
+      context.delete('error_backtrace')
+      context.delete('error_message')
+      context.delete('error_class')
+
+      Rollbar.report_exception(e, :params => context)
+    end
   end
 end
