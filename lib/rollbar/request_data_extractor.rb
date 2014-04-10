027d3b35a16a5070d5fc8297bf6ee88b75cd8058
Paul Menchov <paul@angel.co>
2014-04-10 13:43:41 -0700

    memoize sensitive params regexp

--- lib/rollbar/request_data_extractor.rb
+++ lib/rollbar/request_data_extractor.rb
@@ -119,12 +119,12 @@ module Rollbar
     end
 
     def rollbar_filtered_params(sensitive_params, params)
-      sensitive_params_regexp = Regexp.new(sensitive_params.map(&:to_s).join('|'), true)
+      @sensitive_params_regexp ||= Regexp.new(sensitive_params.map(&:to_s).join('|'), true)
       if params.nil?
         {}
       else
         params.to_hash.inject({}) do |result, (key, value)|
-          if sensitive_params_regexp =~ key.to_s
+          if @sensitive_params_regexp =~ key.to_s
             result[key] = rollbar_scrubbed(value)
           elsif value.is_a?(Hash)
             result[key] = rollbar_filtered_params(sensitive_params, value)
