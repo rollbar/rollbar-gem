8b0592b41dcebe9f43040c0ffc81d528726fef50
Jon de Andres <jon@wuaki.tv>
2014-10-05 03:46:20 +0200

    Use new Rollbar.scoped method in Rails middleware.

--- lib/rollbar/middleware/rails/rollbar.rb
+++ lib/rollbar/middleware/rails/rollbar.rb
@@ -4,55 +4,51 @@ module Rollbar
       class RollbarMiddleware
         include RequestDataExtractor
         include ExceptionReporter
-        
+
         def initialize(app)
           @app = app
         end
-        
+
+        def person_data_proc(env)
+          proc do
+            ActiveRecord::Base.connection_pool.with_connection do
+              extract_person_data_from_controller(env)
+            end
+          end
+        end
+
+        def context(request_data)
+          return unless request_data[:route]
+
+          route = request_data[:route]
+          # make sure route is a hash built by RequestDataExtractor
+          return "#{route[:controller]}" + '#' + "#{route[:action]}" if route.is_a?(Hash) && !route.empty?
+        end
+
         def call(env)
           begin
             request_data = extract_request_data_from_rack(env)
-            
-            person_data = Proc.new do
-              ActiveRecord::Base.connection_pool.with_connection do
-                extract_person_data_from_controller(env)
-              end
-            end
-            
-            context = nil
-            
-            if request_data[:route]
-              route = request_data[:route]
 
-              # make sure route is a hash built by RequestDataExtractor
-              if route.is_a?(Hash) and not route.empty?
-                context = "#{route[:controller]}" + '#' + "#{route[:action]}"
-              end
-            end
-            
             # Scope a new notifier with request data and a Proc for person data
             # for any reports that happen while a controller is handling a request
-            Thread.current[:_rollbar_notifier] = Rollbar.scope({
+            rollbar_scope = {
               :request => request_data,
-              :person => person_data,
-              :context => context
-            })
-            
-            response = @app.call(env)
+              :person => person_data_proc(env),
+              :context => context(request_data)
+            }
+
+            response = Rollbar.scoped(rollbar_scope) { @app.call(env) }
           rescue Exception => exception
             report_exception_to_rollbar(env, exception)
-            
-            Thread.current[:_rollbar_notifier] = nil
-            
+            Rollbar.reset_notifier!
+
             raise
           end
-          
-          if env["rack.exception"]
-            report_exception_to_rollbar(env, env["rack.exception"])
-          end
-          
-          Thread.current[:_rollbar_notifier] = nil
-          
+
+          report_exception_to_rollbar(env, env["rack.exception"]) if env["rack.exception"]
+
+          Rollbar.reset_notifier!
+
           response
         end
       end
