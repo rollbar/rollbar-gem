58e0be15f40a8782279682f0a8b0c4ee4e6f68ce
Jon de Andres <jon@wuaki.tv>
2014-11-25 18:45:49 +0100

    Add helper to load and parse json payloads.

--- spec/support/fixture_helpers.rb
+++ spec/support/fixture_helpers.rb
@@ -3,4 +3,8 @@ module FixtureHelpers
     root = File.expand_path('../../fixtures', __FILE__)
     File.join(root, relative_path)
   end
+
+  def load_payload_fixture(relative_path)
+    MultiJson.load(File.read(fixture_file(relative_path)))
+  end
 end
