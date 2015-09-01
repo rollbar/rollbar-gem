4d246322902ce3b4fe585a57f794bbedb6f90055
Jon de Andres <jon@rollbar.com>
2015-09-01 02:27:32 +0200

    Speed up encoding.

--- lib/rollbar/encoding.rb
+++ lib/rollbar/encoding.rb
@@ -1,21 +1,25 @@
 module Rollbar
   module Encoding
+    class << self
+      attr_accessor :encoding_class
+    end
+
     def self.encode(object)
       can_be_encoded = object.is_a?(Symbol) || object.is_a?(String)
 
+      return if object.frozen?
       return object unless can_be_encoded
 
       encoding_class.new(object).encode
     end
-
-    def self.encoding_class
-      if String.instance_methods.include?(:encode)
-        require 'rollbar/encoding/encoder'
-        Encoder
-      else
-        require 'rollbar/encoding/legacy_encoder'
-        LegacyEncoder
-      end
-    end
   end
 end
+
+if String.instance_methods.include?(:encode)
+  require 'rollbar/encoding/encoder'
+  Rollbar::Encoding.encoding_class = Rollbar::Encoding::Encoder
+else
+  Rollbar::Encoding.encoding_class = Rollbar::Encoding::LegacyEncoder
+  require 'rollbar/encoding/legacy_encoder'
+end
+
--- lib/rollbar/encoding/encoder.rb
+++ lib/rollbar/encoding/encoder.rb
@@ -4,6 +4,8 @@ module Rollbar
       ALL_ENCODINGS = [::Encoding::UTF_8, ::Encoding::ISO_8859_1, ::Encoding::ASCII_8BIT, ::Encoding::US_ASCII]
       ASCII_ENCODINGS = [::Encoding::US_ASCII, ::Encoding::ASCII_8BIT, ::Encoding::ISO_8859_1]
       ENCODING_OPTIONS = { :invalid => :replace, :undef => :replace, :replace => '' }
+      UTF8 = 'UTF-8'.freeze
+      BINARY = 'binary'.freeze
 
       attr_accessor :object
 
@@ -12,9 +14,15 @@ module Rollbar
       end
 
       def encode
-        value = object.to_s
+        value = object.to_s.dup
+        encoding = value.encoding
 
-        encoded_value = force_encoding(value).encode(*encoding_args(value))
+        # This will be most of cases so avoid force anything for them
+        if encoding == ::Encoding::UTF_8 && value.valid_encoding?
+          encoded_value = value
+        else
+          encoded_value = force_encoding(value).encode(*encoding_args(value))
+        end
 
         object.is_a?(Symbol) ? encoded_value.to_sym : encoded_value
       end
@@ -22,8 +30,6 @@ module Rollbar
       private
 
       def force_encoding(value)
-        return value if value.frozen?
-
         value.force_encoding(detect_encoding(value)) if value.encoding == ::Encoding::UTF_8
 
         value
@@ -43,8 +49,8 @@ module Rollbar
       end
 
       def encoding_args(value)
-        args = ['UTF-8']
-        args << 'binary' if ASCII_ENCODINGS.include?(value.encoding)
+        args = [UTF8]
+        args << BINARY if ASCII_ENCODINGS.include?(value.encoding)
         args << ENCODING_OPTIONS
 
         args
