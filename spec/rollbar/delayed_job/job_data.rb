96676a172cfcc02fe34ce5bdce12cb64250d347a
Jon de Andres <jon@rollbar.com>
2015-09-15 18:09:03 +0200

    Send correct hash value for delayed job 'handler' object.
    It'll be useful for users send a Hash object there instead sending a
    YAML string.

---
+++ spec/rollbar/delayed_job/job_data.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+require 'rollbar/delayed_job'
+require 'delayed/backend/test'
+
+describe Rollbar::Delayed::JobData do
+  describe '#to_hash' do
+    let(:handler) { { 'foo' => 'bar' } }
+
+    let(:attrs) do
+      {
+        'id' => 1,
+        'priority' => 0,
+        'attempts' => 1,
+        'handler' => handler.to_yaml
+      }
+    end
+
+    let(:job) do
+      ::Delayed::Backend::Test::Job.new(attrs)
+    end
+
+    subject { described_class.new(job) }
+
+    it 'returns the correct job data' do
+      expected_result = attrs.dup
+      expected_result.delete('id')
+      expected_result['handler'] = handler
+
+      result = subject.to_hash
+
+      expect(result).to be_eql(expected_result)
+    end
+  end
+end
