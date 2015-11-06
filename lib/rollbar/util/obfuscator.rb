a2202b8dcdb7bb355b4074b8fa8bb22488d8f7ce
Robert Steuck <robert.steuck@gmail.com>
2015-11-06 15:35:38 +0100

    refactor ip obfuscator

--- lib/rollbar/util/obfuscator.rb
+++ lib/rollbar/util/obfuscator.rb
@@ -5,20 +5,16 @@ module Rollbar
       require 'digest'
 
       def self.obfuscate_ip(ip_string)
-        if Rollbar.configuration.user_ip_obfuscator_secret
-          begin
-            secret = Rollbar.configuration.user_ip_obfuscator_secret
-            ip_int32 = IPAddr.new(ip_string, Socket::AF_INET).to_i
-            secret_int32 = Digest::MD5.hexdigest(secret)[0..7].to_i(16)
-            obfuscated_ip_int32 = ip_int32 ^ secret_int32 % (2 << 31)
+        return ip_string unless Rollbar.configuration.user_ip_obfuscator_secret
 
-            IPAddr.new(obfuscated_ip_int32, Socket::AF_INET).to_s
-          rescue
-            nil
-          end
-        else
-          ip_string
-        end
+        secret = Rollbar.configuration.user_ip_obfuscator_secret
+        ip_int32 = IPAddr.new(ip_string, Socket::AF_INET).to_i
+        secret_int32 = Digest::MD5.hexdigest(secret)[0..7].to_i(16)
+        obfuscated_ip_int32 = ip_int32 ^ secret_int32 % (2 << 31)
+
+        IPAddr.new(obfuscated_ip_int32, Socket::AF_INET).to_s
+      rescue
+        nil
       end
     end
   end
