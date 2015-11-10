e98e0f16317a7c01ab1f8d8bb4cde8ebe11eda99
Jon de Andres <jon@rollbar.com>
2015-11-10 11:15:23 +0100

    Rename Obfuscator to IPObfuscator
    And require it in request_data_extractor.rb
    
    @hqm42

--- lib/rollbar/request_data_extractor.rb
+++ lib/rollbar/request_data_extractor.rb
@@ -2,6 +2,7 @@ require 'rack'
 require 'tempfile'
 
 require 'rollbar/scrubbers/url'
+require 'rollbar/util/ip_obfuscator'
 
 module Rollbar
   module RequestDataExtractor
@@ -118,7 +119,7 @@ module Rollbar
     def rollbar_user_ip(env)
       user_ip_string = (env['action_dispatch.remote_ip'] || env['HTTP_X_REAL_IP'] || env['HTTP_X_FORWARDED_FOR'] || env['REMOTE_ADDR']).to_s
 
-      Rollbar::Util::Obfuscator.obfuscate_ip(user_ip_string)
+      Rollbar::Util::IPObfuscator.obfuscate_ip(user_ip_string)
     rescue
       nil
     end
