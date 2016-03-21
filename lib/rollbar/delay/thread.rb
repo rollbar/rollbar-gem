cab7ba7b5cf512fdb3c0ad20d2c0886be33bfc48
Alexander Menzhinsky <amenzhinsky@gmail.com>
2016-03-21 11:57:11 +0300

    Add threads reaper

--- lib/rollbar/delay/thread.rb
+++ lib/rollbar/delay/thread.rb
@@ -1,9 +1,64 @@
+require 'thread'
+require 'timeout'
+
 module Rollbar
   module Delay
     class Thread
-      def self.call(payload)
-        new.call(payload)
-      end
+      EXIT_SIGNAL  = :exit
+      EXIT_TIMEOUT = 3
+
+      Error        = Class.new(StandardError)
+      TimeoutError = Class.new(Error)
+
+      class << self
+        attr_reader :reaper
+
+        def call(payload)
+          spawn_threads_reaper
+          threads << new.call(payload)
+        end
+
+        private
+
+        def threads
+          @threads ||= Queue.new
+        end
+
+        def spawn_threads_reaper
+          return if @spawned
+          @spawned = true
+
+          @reaper ||= build_reaper_thread
+          configure_exit_handler
+        end
+
+        def build_reaper_thread
+          ::Thread.start do
+            loop do
+              thread = threads.pop
+
+              if thread == EXIT_SIGNAL
+                break
+              else
+                thread.join
+              end
+            end
+          end
+        end
+
+        def configure_exit_handler
+          at_exit do
+            begin
+              Timeout.timeout(EXIT_TIMEOUT) do
+                threads << EXIT_SIGNAL
+                reaper.join
+              end
+            rescue Timeout::Error
+              raise TimeoutError, "unable to reap all threads within #{EXIT_TIMEOUT} seconds"
+            end
+          end
+        end
+      end # class << self
 
       def call(payload)
         ::Thread.new do
