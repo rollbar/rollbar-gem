d35d2d16a3e7f68a15fdf31893ba663f6056d8c2
Jon de Andres <jon@rollbar.com>
2016-04-08 18:58:23 +0200

    Clean scope before every Sidekiq job execution
    Fix #416

--- lib/rollbar/sidekiq.rb
+++ lib/rollbar/sidekiq.rb
@@ -4,6 +4,14 @@ module Rollbar
   class Sidekiq
     PARAM_BLACKLIST = %w[backtrace error_backtrace error_message error_class]
 
+    class ClearScope
+      def call(worker, msg, queue)
+        Rollbar.reset_notifier!
+
+        yield
+      end
+    end
+
     def self.handle_exception(msg_or_context, e)
       return if skip_report?(msg_or_context, e)
 
@@ -14,11 +22,13 @@ module Rollbar
     end
 
     def self.skip_report?(msg_or_context, e)
-      msg_or_context.is_a?(Hash) && msg_or_context["retry"] && 
+      msg_or_context.is_a?(Hash) && msg_or_context["retry"] &&
         msg_or_context["retry_count"] && msg_or_context["retry_count"] < ::Rollbar.configuration.sidekiq_threshold
     end
 
     def call(worker, msg, queue)
+      Rollbar.reset_notifier!
+
       yield
     rescue Exception => e
       Rollbar::Sidekiq.handle_exception(msg, e)
@@ -28,12 +38,13 @@ module Rollbar
 end
 
 Sidekiq.configure_server do |config|
-  if Sidekiq::VERSION < '3'
+  if Sidekiq::VERSION.split('.')[0].to_i < 3
     config.server_middleware do |chain|
       chain.add Rollbar::Sidekiq
     end
   else
-    config.error_handlers << Proc.new do |e, context|
+    chain.add Rollbar::Sidekiq::ClearScope
+    config.error_handlers << proc do |e, context|
       Rollbar::Sidekiq.handle_exception(context, e)
     end
   end
