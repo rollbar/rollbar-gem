501d111b06ef22eaf0d57e010133ea3b4ffe3e02
Jon de Andres <jon@rollbar.com>
2016-09-02 18:21:29 +0200

    Make Codeclimate happy with Resque Failure class

--- lib/rollbar/plugins/resque/failure.rb
+++ lib/rollbar/plugins/resque/failure.rb
@@ -2,13 +2,16 @@ require 'rollbar'
 
 module Resque
   module Failure
+    # Falure class to use in Resque in order to send
+    # Resque errors to the Rollbar API
     class Rollbar < Base
       def save
-        if use_exception_level_filters?
-          payload_with_options = payload.merge(:use_exception_level_filters => true)
-        else
-          payload_with_options = payload
-        end
+        payload_with_options =
+          if use_exception_level_filters?
+            payload.merge(:use_exception_level_filters => true)
+          else
+            payload
+          end
 
         rollbar.error(exception, payload_with_options)
       end
