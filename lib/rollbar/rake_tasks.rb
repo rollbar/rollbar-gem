1a9f37b7d52618e1bda6aa5c5b8865909e18eb11
Jon de Andres <jon@rollbar.com>
2016-09-16 18:03:22 +0200

    Fix test rake task when Rack::MockRequest is not defined
    Fix #504

--- lib/rollbar/rake_tasks.rb
+++ lib/rollbar/rake_tasks.rb
@@ -1,5 +1,8 @@
 require 'rollbar'
-require 'rack/mock'
+begin
+  require 'rack/mock'
+rescue LoadError
+end
 require 'logger'
 
 namespace :rollbar do
@@ -39,60 +42,62 @@ namespace :rollbar do
       end
     end
 
-    if defined?(Rails)
-      begin
-        require './app/controllers/application_controller'
-      rescue LoadError
-      end
-
-      unless defined?(ApplicationController)
-        puts 'No ApplicationController found, using ActionController::Base instead'
-        class ApplicationController < ActionController::Base; end
-      end
-
-      puts 'Setting up the controller.'
-
-      class RollbarTestController < ApplicationController
-        include RollbarTest
-
-        def verify
-          test_rollbar
+    if defined?(Rack::MockRequest)
+      if defined?(Rails)
+        begin
+          require './app/controllers/application_controller'
+        rescue LoadError
         end
 
-        def logger
-          nil
+        unless defined?(ApplicationController)
+          puts 'No ApplicationController found, using ActionController::Base instead'
+          class ApplicationController < ActionController::Base; end
+        end
+
+        puts 'Setting up the controller.'
+
+        class RollbarTestController < ApplicationController
+          include RollbarTest
+
+          def verify
+            test_rollbar
+          end
+
+          def logger
+            nil
+          end
+        end
+
+        Rails.application.routes_reloader.execute_if_updated
+        Rails.application.routes.draw do
+          get 'verify' => 'rollbar_test#verify', :as => 'verify'
+        end
+
+        # from http://stackoverflow.com/questions/5270835/authlogic-activation-problems
+        if defined? Authlogic
+          Authlogic::Session::Base.controller = Authlogic::ControllerAdapters::RailsAdapter.new(self)
+        end
+
+        protocol = (defined? Rails.application.config.force_ssl && Rails.application.config.force_ssl) ? 'https' : 'http'
+        app = Rails.application
+      else
+        protocol = 'http'
+        app = Class.new do
+          include RollbarTest
+
+          def self.call(_env)
+            new.test_rollbar
+          end
         end
       end
 
-      Rails.application.routes_reloader.execute_if_updated
-      Rails.application.routes.draw do
-        get 'verify' => 'rollbar_test#verify', :as => 'verify'
+      puts 'Processing...'
+      env = Rack::MockRequest.env_for("#{protocol}://www.example.com/verify")
+      status, = app.call(env)
+
+      unless status.to_i == 500
+        puts 'Test failed! You may have a configuration issue, or you could be using a gem that\'s blocking the test. Contact support@rollbar.com if you need help troubleshooting.'
       end
-
-      # from http://stackoverflow.com/questions/5270835/authlogic-activation-problems
-      if defined? Authlogic
-        Authlogic::Session::Base.controller = Authlogic::ControllerAdapters::RailsAdapter.new(self)
-      end
-
-      protocol = (defined? Rails.application.config.force_ssl && Rails.application.config.force_ssl) ? 'https' : 'http'
-      app = Rails.application
-    else
-      protocol = 'http'
-      app = Class.new do
-        include RollbarTest
-
-        def self.call(_env)
-          new.test_rollbar
-        end
-      end
-    end
-
-    puts 'Processing...'
-    env = Rack::MockRequest.env_for("#{protocol}://www.example.com/verify")
-    status, = app.call(env)
-
-    unless status.to_i == 500
-      puts 'Test failed! You may have a configuration issue, or you could be using a gem that\'s blocking the test. Contact support@rollbar.com if you need help troubleshooting.'
     end
   end
 end
