175f82fc3dd958ddeecb31a0b34a6b49843d9021
Valentin Ruskevych <leaderpvp@gmail.com>
2017-03-15 16:46:27 +0200

    added use_shoryuken and shuryuken delay job.

--- lib/rollbar/delay/shoryuken.rb
+++ lib/rollbar/delay/shoryuken.rb
@@ -1,28 +1,27 @@
-require 'sidekiq'
+require 'shoryuken'
 
 module Rollbar
   module Delay
-    class Sidekiq
-      OPTIONS = { 'queue' => 'rollbar', 'class' => Rollbar::Delay::Sidekiq }.freeze
+    class Shoryuken
+      include ::Shoryuken::Worker
 
-      def initialize(*args)
-        @options = (opts = args.shift) ? OPTIONS.merge(opts) : OPTIONS
-      end
+      # not allowing bulk, to not double-report rollbars if one of them failed in bunch.
+      shoryuken_options queue: ->{ "rollbar_#{Rollbar.configuration.environment.to_s}" }, auto_delete: true, body_parser: :json, retry_intervals: [60, 180, 360, 1200, 3600, 18600]
 
-      def call(payload)
-        ::Sidekiq::Client.push @options.merge('args' => [payload])
-      end
-
-      include ::Sidekiq::Worker
-
+      ## responsible for performing job
       def perform(*args)
         begin
           Rollbar.process_from_async_handler(*args)
         rescue
-          # Raise the exception so Sidekiq can track the errored job
+          # Raise the exception so Shoryuken can track the errored job
           # and retry it
           raise
         end
+      end
+
+      ## to push the job !
+      def call(payload)
+        self.class.perform_async(payload)
       end
     end
   end
