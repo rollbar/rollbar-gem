e48aa66cfb604b1cddac0eb548d745d568172b7e
Valentin Ruskevych <leaderpvp@gmail.com>
2017-06-08 15:29:36 +0300

    fixes of syntax to satisfy rubocops requirements.

--- lib/rollbar/delay/shoryuken.rb
+++ lib/rollbar/delay/shoryuken.rb
@@ -6,15 +6,15 @@ module Rollbar
       include ::Shoryuken::Worker
 
       # not allowing bulk, to not double-report rollbars if one of them failed in bunch.
-      shoryuken_options queue: -> { queue_name }, auto_delete: true, body_parser: :json, retry_intervals: [60, 180, 360, 1200, 3600, 18600]
+      shoryuken_options queue: -> { queue_name }, :auto_delete => true, :body_parser => :json, :retry_intervals => [60, 180, 360, 120_0, 360_0, 186_00]
 
       def self.queue_name
-        "rollbar_#{Rollbar.configuration.environment.to_s}"
+        "rollbar_#{Rollbar.configuration.environment}"
       end
 
       ## responsible for performing job. - payload is a json parsed body of the message.
-      def perform(sqs_message, payload)
-          Rollbar.process_from_async_handler(payload)
+      def perform(_sqs_message, payload)
+        Rollbar.process_from_async_handler(payload)
       end
 
       ## to push the job !
