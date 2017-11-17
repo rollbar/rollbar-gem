a23e9f3f9c657174939174b24a5e7b0a2c0ee110
Andrew Weiss <wvvwwvw@gmail.com>
2017-11-17 11:52:31 -0800

    Get tests to pass on Rails 5.1 (#652)
    * Make a rails51.gemfile for travis
    
    * get travis to actually run rails51
    
    * make 5.1 only build on 2.2.2+
    
    * s/before_filter/before_action/
    
    * support rails3 and rails 4+
    
    * support both styles of controller tests
    
    * support both styles of integration tests
    
    * fix codacy issues

--- spec/dummyapp/app/controllers/users_controller.rb
+++ spec/dummyapp/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
+  respond_to?(:before_action) ? (before_action :authenticate_user!) : (before_filter :authenticate_user!)
 
   def index
     @users = User.all
