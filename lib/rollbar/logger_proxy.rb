a6e2abf270807d2cbfc7bddaef3b6f9379dc1583
Justin Bull <justin@wealthsimple.com>
2018-05-16 20:56:45 -0400

    Add Rollbar.configuration.logger_level
    This allows customization where Rollbar may be used inside a Rails project that has a production Rails.logger's log_level set to :info but does not want internal Rollbar entries all over the place.
    
    Just because an app has its default logger set to a particular log level doesn't mean it wants Rollbar's own log level to mimic that.

--- lib/rollbar/logger_proxy.rb
+++ lib/rollbar/logger_proxy.rb
@@ -24,11 +24,21 @@ module Rollbar
 
     def log(level, message)
       return unless Rollbar.configuration.enabled
+      return unless acceptable_levels.include? level.to_sym
 
       @object.send(level, message)
     rescue
       puts "[Rollbar] Error logging #{level}:"
       puts "[Rollbar] #{message}"
+    end
+
+    protected
+
+    def acceptable_levels
+      @acceptable_levels ||= begin
+        levels = %i[debug info warn error]
+        levels[levels.find_index(Rollbar.configuration.logger_level)..-1]
+      end
     end
   end
 end
