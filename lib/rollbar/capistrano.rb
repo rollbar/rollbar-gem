c0188040a341d96db44ce873034adf5b370ae180
Artur Moczulski <artur.moczulski@gmail.com>
2019-01-18 00:46:43 +0000

    782: run rubocop -a to clean up the code

--- lib/rollbar/capistrano.rb
+++ lib/rollbar/capistrano.rb
@@ -8,47 +8,41 @@ require 'json'
 require 'rollbar/capistrano_tasks'
 
 module Rollbar
-    module Capistrano
-      
-        def self.load_into(configuration)
-          configuration.load do
-            
-            before 'deploy', 'rollbar:deploy_started'
-            
-            after 'deploy', 'rollbar:deploy_succeeded'
-            after 'deploy:migrations', 'rollbar:deploy_succeeded'
-            after 'deploy:cold',       'rollbar:deploy_succeeded'
-            
-            _cset(:rollbar_role)  { :app }
-            _cset(:rollbar_user)  { ENV['USER'] || ENV['USERNAME'] }
-            _cset(:rollbar_env)   { fetch(:rails_env, 'production') }
-            _cset(:rollbar_token) { abort("Please specify the Rollbar access token, set :rollbar_token, 'your token'") }
-            _cset(:rollbar_revision) { current_revision }
-            _cset(:rollbar_comment) { nil }
-      
-            namespace :rollbar do
-              
-              desc 'Send deployment started notification to Rollbar.'
-              task :deploy_started do
-                  ::Rollbar::CapistranoTasks.deploy_started(self, self.logger, configuration.method(:dry_run))
-              end
-              
-              desc 'Send deployment succeeded notification to Rollbar.'
-              task :deploy_succeeded do
-                  ::Rollbar::CapistranoTasks.deploy_succeeded(self, self.logger, configuration.method(:dry_run))
-              end
-              
-              desc 'Send deployment failed notification to Rollbar.'
-              task :deploy_failed do
-                  ::Rollbar::CapistranoTasks.deploy_failed(self, self.logger, configuration.method(:dry_run))
-              end
-              
-            end
+  module Capistrano
+    def self.load_into(configuration)
+      configuration.load do
+        before 'deploy', 'rollbar:deploy_started'
+
+        after 'deploy', 'rollbar:deploy_succeeded'
+        after 'deploy:migrations', 'rollbar:deploy_succeeded'
+        after 'deploy:cold',       'rollbar:deploy_succeeded'
+
+        _cset(:rollbar_role)  { :app }
+        _cset(:rollbar_user)  { ENV['USER'] || ENV['USERNAME'] }
+        _cset(:rollbar_env)   { fetch(:rails_env, 'production') }
+        _cset(:rollbar_token) { abort("Please specify the Rollbar access token, set :rollbar_token, 'your token'") }
+        _cset(:rollbar_revision) { current_revision }
+        _cset(:rollbar_comment) { nil }
+
+        namespace :rollbar do
+          desc 'Send deployment started notification to Rollbar.'
+          task :deploy_started do
+            ::Rollbar::CapistranoTasks.deploy_started(self, logger, configuration.method(:dry_run))
+          end
+
+          desc 'Send deployment succeeded notification to Rollbar.'
+          task :deploy_succeeded do
+            ::Rollbar::CapistranoTasks.deploy_succeeded(self, logger, configuration.method(:dry_run))
+          end
+
+          desc 'Send deployment failed notification to Rollbar.'
+          task :deploy_failed do
+            ::Rollbar::CapistranoTasks.deploy_failed(self, logger, configuration.method(:dry_run))
           end
         end
+      end
     end
+  end
 end
 
-if Capistrano::Configuration.instance
-  Rollbar::Capistrano.load_into(Capistrano::Configuration.instance)
-end
+Rollbar::Capistrano.load_into(Capistrano::Configuration.instance) if Capistrano::Configuration.instance
--- lib/rollbar/deploy.rb
+++ lib/rollbar/deploy.rb
@@ -2,9 +2,8 @@ require 'capistrano'
 
 module Rollbar
   module Deploy
-    
-    ENDPOINT = 'https://api.rollbar.com/api/1/deploy/'
-      
+    ENDPOINT = 'https://api.rollbar.com/api/1/deploy/'.freeze
+
     def self.report(
       access_token:,
       environment:,
@@ -15,12 +14,12 @@ module Rollbar
       status: 'started',
       proxy: nil,
       dry_run: false
-      )
-      
+    )
+
       uri = URI.parse(::Rollbar::Deploy::ENDPOINT)
 
       request = Net::HTTP::Post.new(uri.request_uri)
-      request.body = ::JSON.dump({
+      request.body = ::JSON.dump(
         :access_token => access_token,
         :environment => environment,
         :revision => revision,
@@ -28,17 +27,15 @@ module Rollbar
         :local_username => local_username,
         :comment => comment,
         :status => status.to_s
-      })
-      
-      result = self.send_request(uri, proxy, request, dry_run)
-      
-      if result[:response].is_a? Net::HTTPSuccess
-        result[:deploy_id] = JSON.parse(result[:response].body)['data']['deploy_id']
-      end
-      
+      )
+
+      result = send_request(uri, proxy, request, dry_run)
+
+      result[:deploy_id] = JSON.parse(result[:response].body)['data']['deploy_id'] if result[:response].is_a? Net::HTTPSuccess
+
       result
     end
-    
+
     def self.update(
       deploy_id:,
       access_token:,
@@ -46,43 +43,42 @@ module Rollbar
       comment: nil,
       proxy: nil,
       dry_run: false
-      )
-      
+    )
+
       uri = URI.parse(
-        ::Rollbar::Deploy::ENDPOINT + 
+        ::Rollbar::Deploy::ENDPOINT +
         deploy_id.to_s +
-        "?access_token=" + access_token
+        '?access_token=' + access_token
       )
 
       request = Net::HTTP::Patch.new(uri.request_uri)
-      request.body = ::JSON.dump({
+      request.body = ::JSON.dump(
         :status => status.to_s,
         :comment => comment
-      })
-      
-      self.send_request(uri, proxy, request, dry_run)
+      )
+
+      send_request(uri, proxy, request, dry_run)
     end
-      
-  private
-    
+
+    private
+
     def self.send_request(uri, proxy, request, dry_run)
       Net::HTTP.start(uri.host, uri.port, proxy, :use_ssl => true) do |http|
-        
         result = {
-          request_info: uri.inspect + ": " + request.body,
-          request: request
+          :request_info => uri.inspect + ': ' + request.body,
+          :request => request
         }
-        
+
         unless dry_run
           response = http.request(request)
-          
+
           result[:response] = response
-          result[:response_info] = 
-            response.code + "; " + 
-            response.message + "; " + 
+          result[:response_info] =
+            response.code + '; ' +
+            response.message + '; ' +
             response.body.delete!("\n")
         end
-        
+
         result
       end
     end
