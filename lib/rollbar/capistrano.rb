a701add30dddcd555aaa7f0fe89f300b38c1b269
Artur Moczulski <artur.moczulski@gmail.com>
2019-01-18 04:45:24 +0000

    782: descrease ABC in capistrano.rb

--- lib/rollbar/capistrano.rb
+++ lib/rollbar/capistrano.rb
@@ -9,36 +9,69 @@ require 'rollbar/capistrano_tasks'
 
 module Rollbar
   # Module for loading Rollbar Capistrano tasks into Capistrano 2
-  module Capistrano
-    def self.load_into(configuration)
-      configuration.load do
-        before 'deploy', 'rollbar:deploy_started'
+  module Capistrano2
+    class << self
+      def load_into(configuration)
+        load_tasks(configuration)
+        load_tasks_flow(configuration)
+        load_properties(configuration)
+      end
 
-        after 'deploy', 'rollbar:deploy_succeeded'
-        after 'deploy:migrations', 'rollbar:deploy_succeeded'
-        after 'deploy:cold',       'rollbar:deploy_succeeded'
+      private
 
-        _cset(:rollbar_role)  { :app }
-        _cset(:rollbar_user)  { ENV['USER'] || ENV['USERNAME'] }
-        _cset(:rollbar_env)   { fetch(:rails_env, 'production') }
-        _cset(:rollbar_token) { abort("Please specify the Rollbar access token, set :rollbar_token, 'your token'") }
-        _cset(:rollbar_revision) { current_revision }
-        _cset(:rollbar_comment) { nil }
+      def load_tasks_flow(configuration)
+        configuration.load do
+          before 'deploy', 'rollbar:deploy_started'
 
-        namespace :rollbar do
-          desc 'Send deployment started notification to Rollbar.'
-          task :deploy_started do
-            ::Rollbar::CapistranoTasks.deploy_started(self, logger, configuration.method(:dry_run))
-          end
+          after 'deploy', 'rollbar:deploy_succeeded'
+          after 'deploy:migrations', 'rollbar:deploy_succeeded'
+          after 'deploy:cold',       'rollbar:deploy_succeeded'
+        end
+      end
 
-          desc 'Send deployment succeeded notification to Rollbar.'
-          task :deploy_succeeded do
-            ::Rollbar::CapistranoTasks.deploy_succeeded(self, logger, configuration.method(:dry_run))
-          end
+      def load_properties(configuration)
+        configuration.load do
+          _cset(:rollbar_role)  { :app }
+          _cset(:rollbar_user)  { ENV['USER'] || ENV['USERNAME'] }
+          _cset(:rollbar_env)   { fetch(:rails_env, 'production') }
+          _cset(:rollbar_token) { abort("Please specify the Rollbar access token, set :rollbar_token, 'your token'") }
+          _cset(:rollbar_revision) { current_revision }
+          _cset(:rollbar_comment) { nil }
+        end
+      end
 
-          desc 'Send deployment failed notification to Rollbar.'
-          task :deploy_failed do
-            ::Rollbar::CapistranoTasks.deploy_failed(self, logger, configuration.method(:dry_run))
+      def load_tasks(configuration)
+        load_deploy_started(configuration)
+        load_deploy_succeeded(configuration)
+      end
+
+      def load_deploy_started(configuration)
+        load_task(
+          :desc => 'Send deployment started notification to Rollbar.',
+          :task => :deploy_started,
+          :configuration => configuration
+        ) do
+          ::Rollbar::CapistranoTasks.deploy_started(configuration, configuration.logger, configuration.dry_run)
+        end
+      end
+
+      def load_deploy_succeeded(configuration)
+        load_task(
+          :desc => 'Send deployment succeeded notification to Rollbar.',
+          :task => :deploy_succeeded,
+          :configuration => configuration
+        ) do
+          ::Rollbar::CapistranoTasks.deploy_succeeded(configuration, configuration.logger, configuration.dry_run)
+        end
+      end
+
+      def load_task(configuration:, desc:, task:)
+        configuration.load do
+          namespace :rollbar do
+            desc(desc)
+            task(task) do
+              yield
+            end
           end
         end
       end
@@ -46,4 +79,4 @@ module Rollbar
   end
 end
 
-Rollbar::Capistrano.load_into(Capistrano::Configuration.instance) if Capistrano::Configuration.instance
+Rollbar::Capistrano2.load_into(Capistrano::Configuration.instance) if Capistrano::Configuration.instance
--- lib/rollbar/capistrano_tasks.rb
+++ lib/rollbar/capistrano_tasks.rb
@@ -5,122 +5,125 @@ require 'rollbar/deploy'
 module Rollbar
   # Module containing the logic of Capistrano tasks for deploy tracking
   module CapistranoTasks
-    def self.deploy_started(capistrano, logger, dry_run_proc)
-      logger.warn("You need to upgrade capistrano to '>= 3.1' version in order to correctly report deploys to Rollbar. (On 3.0, the reported revision will be incorrect.)") if ::Capistrano::VERSION =~ /^3\.0/
-
-      logger.info 'Notifying Rollbar of deployment start'
-
-      result = ::Rollbar::Deploy.report(
-        {
-          rollbar_username: capistrano.fetch(:rollbar_user),
-          local_username: capistrano.fetch(:rollbar_user),
-          comment: capistrano.fetch(:rollbar_comment),
-          status: :started,
-          proxy: :ENV,
-          dry_run: dry_run_proc.call
-        },
-        :access_token => capistrano.fetch(:rollbar_token),
-        :environment => capistrano.fetch(:rollbar_env),
-        :revision => capistrano.fetch(:rollbar_revision)
-      )
-
-      logger.info result[:request_info]
-
-      if dry_run_proc.call
-
-        capistrano.set :rollbar_deploy_id, 123
-        logger.info 'Skipping sending HTTP requests to Rollbar in dry run.'
-
-      else
-
-        logger.info result[:response_info] if result[:response_info]
-
-        if deploy_id = result[:data][:deploy_id]
-          capistrano.set :rollbar_deploy_id, deploy_id
-        else
-          logger.error 'Unable to report deploy to Rollbar'
-        end
-
-      end
-    end
-
-    def self.deploy_succeeded(capistrano, logger, dry_run_proc)
-      logger.warn("You need to upgrade capistrano to '>= 3.1' version in order to correctly report deploys to Rollbar. (On 3.0, the reported revision will be incorrect.)") if ::Capistrano::VERSION =~ /^3\.0/
-
-      logger.info 'Setting deployment status to `succeeded` in Rollbar'
-
-      deploy_id = capistrano.fetch(:rollbar_deploy_id)
-
-      if deploy_id
-        result = ::Rollbar::Deploy.update(
+    class << self
+      
+      def deploy_started(capistrano, logger, dry_run)
+        logger.warn("You need to upgrade capistrano to '>= 3.1' version in order to correctly report deploys to Rollbar. (On 3.0, the reported revision will be incorrect.)") if ::Capistrano::VERSION =~ /^3\.0/
+  
+        logger.info 'Notifying Rollbar of deployment start'
+  
+        result = ::Rollbar::Deploy.report(
           {
+            rollbar_username: capistrano.fetch(:rollbar_user),
+            local_username: capistrano.fetch(:rollbar_user),
+            comment: capistrano.fetch(:rollbar_comment),
+            status: :started,
             proxy: :ENV,
-            dry_run: dry_run_proc.call
+            dry_run: dry_run
           },
           :access_token => capistrano.fetch(:rollbar_token),
-          :deploy_id => deploy_id,
-          :status => :succeeded
+          :environment => capistrano.fetch(:rollbar_env),
+          :revision => capistrano.fetch(:rollbar_revision)
         )
-
+  
         logger.info result[:request_info]
-
-        logger.info result[:response_info] if result[:response_info]
-
-        if dry_run_proc.call
-
+  
+        if dry_run
+  
+          capistrano.set :rollbar_deploy_id, 123
           logger.info 'Skipping sending HTTP requests to Rollbar in dry run.'
-
-        else 
-
-          if result[:response].is_a? Net::HTTPSuccess
-            logger.info 'Set deployment status to `succeeded` in Rollbar'
-          else
-            logger.error 'Unable to update deploy status in Rollbar'
-          end
-
-        end
-      else
-        logger.error 'Failed to update the deploy in Rollbar. No deploy id available.'
-      end
-    end
-
-    def self.deploy_failed(capistrano, logger, dry_run_proc)
-      logger.warn("You need to upgrade capistrano to '>= 3.1' version in order to correctly report deploys to Rollbar. (On 3.0, the reported revision will be incorrect.)") if ::Capistrano::VERSION =~ /^3\.0/
-
-      logger.info 'Setting deployment status to `failed` in Rollbar'
-
-      deploy_id = capistrano.fetch(:rollbar_deploy_id)
-
-      if deploy_id
-        result = ::Rollbar::Deploy.update(
-          {
-            proxy: :ENV,
-            dry_run: dry_run_proc.call
-          },
-          :access_token => capistrano.fetch(:rollbar_token),
-          :deploy_id => deploy_id,
-          :status => :failed,
-        )
-
-        logger.info result[:request_info]
-
-        logger.info result[:response_info] if result[:response_info]
-
-        if dry_run_proc.call
-
-          logger.info 'Skipping sending HTTP requests to Rollbar in dry run.'
-
+  
         else
-
-          if result[:response].is_a? Net::HTTPSuccess
-            logger.info 'Set deployment status to `failed` in Rollbar'
+  
+          logger.info result[:response_info] if result[:response_info]
+  
+          if deploy_id = result[:data][:deploy_id]
+            capistrano.set :rollbar_deploy_id, deploy_id
           else
-            logger.error 'Unable to update deploy status in Rollbar'
+            logger.error 'Unable to report deploy to Rollbar'
           end
-
+  
         end
-      else
-        logger.error 'Failed to update the deploy in Rollbar. No deploy id available.'
+      end
+  
+      def deploy_succeeded(capistrano, logger, dry_run)
+        logger.warn("You need to upgrade capistrano to '>= 3.1' version in order to correctly report deploys to Rollbar. (On 3.0, the reported revision will be incorrect.)") if ::Capistrano::VERSION =~ /^3\.0/
+  
+        logger.info 'Setting deployment status to `succeeded` in Rollbar'
+  
+        deploy_id = capistrano.fetch(:rollbar_deploy_id)
+  
+        if deploy_id
+          result = ::Rollbar::Deploy.update(
+            {
+              proxy: :ENV,
+              dry_run: dry_run
+            },
+            :access_token => capistrano.fetch(:rollbar_token),
+            :deploy_id => deploy_id,
+            :status => :succeeded
+          )
+  
+          logger.info result[:request_info]
+  
+          logger.info result[:response_info] if result[:response_info]
+  
+          if dry_run
+  
+            logger.info 'Skipping sending HTTP requests to Rollbar in dry run.'
+  
+          else 
+  
+            if result[:response].is_a? Net::HTTPSuccess
+              logger.info 'Set deployment status to `succeeded` in Rollbar'
+            else
+              logger.error 'Unable to update deploy status in Rollbar'
+            end
+  
+          end
+        else
+          logger.error 'Failed to update the deploy in Rollbar. No deploy id available.'
+        end
+      end
+  
+      def deploy_failed(capistrano, logger, dry_run)
+        logger.warn("You need to upgrade capistrano to '>= 3.1' version in order to correctly report deploys to Rollbar. (On 3.0, the reported revision will be incorrect.)") if ::Capistrano::VERSION =~ /^3\.0/
+  
+        logger.info 'Setting deployment status to `failed` in Rollbar'
+  
+        deploy_id = capistrano.fetch(:rollbar_deploy_id)
+  
+        if deploy_id
+          result = ::Rollbar::Deploy.update(
+            {
+              proxy: :ENV,
+              dry_run: dry_run
+            },
+            :access_token => capistrano.fetch(:rollbar_token),
+            :deploy_id => deploy_id,
+            :status => :failed,
+          )
+  
+          logger.info result[:request_info]
+  
+          logger.info result[:response_info] if result[:response_info]
+  
+          if dry_run
+  
+            logger.info 'Skipping sending HTTP requests to Rollbar in dry run.'
+  
+          else
+  
+            if result[:response].is_a? Net::HTTPSuccess
+              logger.info 'Set deployment status to `failed` in Rollbar'
+            else
+              logger.error 'Unable to update deploy status in Rollbar'
+            end
+  
+          end
+        else
+          logger.error 'Failed to update the deploy in Rollbar. No deploy id available.'
+        end
       end
     end
   end
