8652d55e253b0222c8cea8e417a845c27a122685
Artur Moczulski <artur.moczulski@gmail.com>
2019-01-24 16:38:13 +0000

    759: add test for Rollbar::Delay::ActiveJob

--- spec/rollbar/delay/active_job_spec.rb
+++ spec/rollbar/delay/active_job_spec.rb
@@ -2,5 +2,12 @@ require 'spec_helper'
 require 'rollbar/delay/active_job'
 
 describe Rollbar::Delay::ActiveJob do
-  # to do
+  describe '.call' do
+    let(:payload) { {} }
+    it 'calls Rollbar' do
+      expect(Rollbar).to receive(:process_from_async_handler).with(payload)
+
+      Rollbar::Delay::ActiveJob.call(payload)
+    end
+  end
 end
