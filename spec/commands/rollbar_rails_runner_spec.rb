6ba4d1423fc33002f1ae055de73d3d1e64af2662
Walt Jones <waltjones@gmail.com>
2020-11-06 09:50:35 -0500

    fix: remove travis workarounds for rails test env

--- spec/commands/rollbar_rails_runner_spec.rb
+++ spec/commands/rollbar_rails_runner_spec.rb
@@ -3,13 +3,8 @@ require 'spec_helper'
 if defined?(RSpecCommand)
   require 'rails/rollbar_runner'
 
-  # rspec_command and/or mixlib/shellout are no longer picking up the
-  # environment correctly with Ruby 2.6.x on travis. Not sure why yet.
   describe 'rollbar-rails-runner', :if => RUBY_VERSION < '2.6' do
-    # We set ENV['CURRENT_GEMFILE'] in the gemfile because Travis doesn't set
-    # ENV['BUNDLE_GEMFILE'] correctly.
     command %q[rollbar-rails-runner "puts 'hello'"]
-    environment :BUNDLE_GEMFILE => ENV['CURRENT_GEMFILE']
     its(:stdout) do
       is_expected.to include('hello')
     end
