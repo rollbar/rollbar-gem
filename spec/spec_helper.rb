9b3efee624b074694d2f302a07a4867ae0af0c66
Mateusz Górniak <mateusz@lemonfrog.ch>
2025-01-09 00:32:30 +0100

    Fix deprecation warning for SimpleCov formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
 begin
   require 'simplecov'
 
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::HTMLFormatter
-  ]
+  SimpleCov.formatter =
+    SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter])
 
   SimpleCov.start do
     add_filter '/spec/'
