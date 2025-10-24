6703cc6971c954cda9409253ccf648098ebb24fe
Wojciech Wnętrzak <wojtek@wnetrzak.pl>
2025-10-24 16:08:53 +0200

    Fix Rails error reporter and AJ integration. (#1190)
    When payload is enqueued with Active Job adapter, it cannot contain object instances as those are not serializable.
    
    [fix https://github.com/rollbar/rollbar-gem/issues/1189]

--- lib/rollbar/plugins/rails/error_subscriber.rb
+++ lib/rollbar/plugins/rails/error_subscriber.rb
@@ -5,6 +5,7 @@ module Rollbar
       return unless handled || Rollbar.configuration.capture_uncaught != false
 
       extra = context.is_a?(Hash) ? context.deep_dup : {}
+      extra[:controller] = extra[:controller].class.name if extra[:controller]&.respond_to?(:class)
       extra[:custom_data_method_context] = source
       Rollbar.log(severity, error, extra)
     end
